503570ec279236052e288618a6aeeee239de8b9b
豊田 真吾 <toyodasinngo@gmail.com>
2025-08-02 00:18:19 +0900

    Support Identifiable

--- Sources/KanaShark/GestureKeyboardResult.swift
+++ Sources/KanaShark/GestureKeyboardResult.swift
@@ -5,9 +5,12 @@
 //  Created by Shingo Toyoda on 2025/04/25.
 //
 
+import Foundation
+
 /// The result of a gesture input on the keyboard.
 /// Contains the recognized text and its confidence score.
-public struct GestureKeyboardResult: Sendable {
+public struct GestureKeyboardResult: Sendable, Identifiable {
+    public let id = UUID()
     /// The recognized text from the gesture.
     public let text: String
     /// The confidence score of the recognition.
